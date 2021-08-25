acd170e5594405d995ecb4d8ea799bd80eb3eda6
Fary58 <farrukhchattha7@gmail.com>
2021-08-25 17:22:19 +0500

    Updated with get current location

--- src/UI/Modal.js
+++ src/UI/Modal.js
@@ -4,28 +4,36 @@ export class Modal {
     this.contentTemplateEl = document.getElementById(contentId);
     this.modalTemplateEl = document.getElementById("modal-template");
   }
+
   show() {
     if ("content" in document.createElement("template")) {
       const modalElements = document.importNode(
         this.modalTemplateEl.content,
         true
       );
-      const modalElement = modalElements.querySelector(".modal");
-      const backdropElement = modalElements.querySelector(".backdrop");
-
-      const templateElements = document.importNode(
+      this.modalElement = modalElements.querySelector(".modal");
+      this.backdropElement = modalElements.querySelector(".backdrop");
+      const contentElement = document.importNode(
         this.contentTemplateEl.content,
         true
       );
 
-      modalElement.appendChild(templateElements);
+      this.modalElement.appendChild(contentElement);
 
-      document.body.insertAdjacentElement("afterbegin", modalElement);
-      document.body.insertAdjacentElement("afterbegin", backdropElement);
+      document.body.insertAdjacentElement("afterbegin", this.modalElement);
+      document.body.insertAdjacentElement("afterbegin", this.backdropElement);
     } else {
+      // fallback code
       alert(this.fallbackText);
     }
   }
 
-  hide() {}
+  hide() {
+    if (this.modalElement) {
+      document.body.removeChild(this.modalElement); // this.modalElement.remove()
+      document.body.removeChild(this.backdropElement);
+      this.modalElement = null;
+      this.backdropElement = null;
+    }
+  }
 }
